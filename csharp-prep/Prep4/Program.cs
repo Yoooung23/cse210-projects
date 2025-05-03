7f36e886a7883468e901c819a34f93b7f1aa6d7d
Yoooung23 <logan7design@gmail.com>
2025-05-03 13:59:52 -0600

    seeya sukkah!

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        while (true)
+        {
+            Console.Write("Enter a number, type 0 if finished: ");
+            int reply; 
+
+            if (int.TryParse(Console.ReadLine(), out reply))
+            {
+                if (reply == 0)
+                {
+                    Console.WriteLine("Your list is: ");
+                    foreach (int num in numbers)
+                    {
+                        Console.WriteLine(num);
+                    }
+                    Console.WriteLine("Thanks! See ya sukkah!");
+                    break;
+                } 
+            }
+            numbers.Add(reply);
+        }
     }
 }
