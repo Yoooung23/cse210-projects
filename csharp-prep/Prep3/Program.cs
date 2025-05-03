e1d087ddddee5b2d184495f067e95561f91f66dd
Yoooung23 <logan7design@gmail.com>
2025-05-03 13:15:10 -0600

    LOOPS!!!

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //loops!!!
+            //while
+            string response = "no";
+
+            while (response == "no")
+            {
+                Console.Write("Do you want to end? (yes/no): ");
+                response = Console.ReadLine();
+            }
+            // //do-while
+            string reply;
+            do
+            {
+                Console.Write("Do you like pizza? ");
+                reply = Console.ReadLine();
+            } while (reply == "yes");
+            
+            //for
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            //for-each
+            List<string> colors = new List<string> { "red", "blue", "red", "blue", "green", "red", "blue" };
+
+            foreach (string color in colors)
+            {
+                Console.WriteLine(color);
+            }
+
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        List<string> animals = new List<string>();
+        List<int> debits = new List<int>();
+
+        animals.Add("Cow");
+        animals.Add("Horse");
+        animals.Add("Chicken");
+        animals.Add("Monkey");
+       
+       foreach (string animal in animals)
+       {
+        Console.WriteLine(animal);
+       }
+
+       int AddNumbers(int a, int b)
+       {
+        int sum = a + b;
+        return sum;
+       }
+       Console.WriteLine(AddNumbers(1, 2));
     }
 }
